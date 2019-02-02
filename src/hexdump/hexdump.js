4c97cb15f7ceb09c89f1cf8d000a1ea77d972a54
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-02-02 16:56:17 -0500

    .

--- src/hexdump/hexdump.js
+++ src/hexdump/hexdump.js
@@ -1,19 +1,18 @@
-export default async (file) => {
+export default async file => {
   let program = await loadRom(file);
   let lines = [];
   for (let i = 0; i < program.length; i += 16) {
     let block = program.slice(i, i + 16);
     lines.push(block);
   }
-  return lines
-}
+  return lines;
+};
 
-const loadRom = (name) => {
+const loadRom = name => {
   return fetch(`../roms/${name}`)
-  .then(res => res.arrayBuffer())
-  .then(data => {
-    let rom = new Uint8Array(data);
-    return rom
-  })
-}
-
+    .then(res => res.arrayBuffer())
+    .then(data => {
+      let rom = new Uint8Array(data);
+      return rom;
+    });
+};
