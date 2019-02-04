7d5e50b873a4c3da3adafcd9472f5cd7682760ff
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-02-04 16:58:02 -0500

    fetch no longer fetches html fromindex.html trying to fetch another file

--- src/main.js
+++ src/main.js
@@ -1,27 +1,34 @@
-import Chip8 from './chip8/chip8';
-import hexdump from './hexdump/hexdump';
-'use strict'
+"use strict"
+import Chip8 from "./chip8/chip8";
+import hexDump from 'hexdump-js';
 
 const chip8 = new Chip8();
 
-chip8.popSelect(chip8.romSelector)
+chip8.popSelect(chip8.romSelector);
 
+chip8.romSelector.addEventListener(
+  "change",
+  e => {
+    if (e.target.value != "") {
+      // console.log(e.target.value)
+      // chip8.loadRom(e.target.value)
+      dissambler(e.target.value);
+    }
+  },
+  false
+);
 
-chip8.romSelector.addEventListener('change',(e) => {
-  if (e.target.value != "") {
-    
-    Rom = chip8.loadRom(e.target.value);
-    // Rom.then(data => {
-    //   // console.log(disassembler(data))
-    // })
-  }
-}, false);
+const loadBuffer = name => {
+  return fetch(`./roms/${name}`)
+    .then(res => res.arrayBuffer())
+    .then(data => {
+      return hexDump(data);
+    });
+};
 
 // console.log(chip8.romSelector.value)
-const dissambler = async () => {
-  let Rom = await chip8.loadRom("INVADERS")
+const dissambler = async (name) => {
+  let Rom = await loadBuffer(name);
   console.log(Rom)
-}
+};
 
-let hex = hexdump('INVADERS')
-console.log(hex)
