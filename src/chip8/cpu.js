ac85a2168fb3dd01e723d99f670a93af655a4a8a
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-02-07 18:02:40 -0500

    implemented more opcodes and tests

--- src/chip8/cpu.js
+++ src/chip8/cpu.js
@@ -2,13 +2,14 @@ export default class Cpu {
   constructor() {
     this.memory = new Uint8Array(4096);
     this.pc = 0x200;
-    this.i = 0;
+    this.iR = 0;
     this.display = this.setArray(new Array(0x800), false);
     this.input = this.setArray(new Array(16), false)
-    this.v = new Uint8Array(0x10);
+    this.vR = new Uint8Array(0x10);
     this.delayTimer = 0;
     this.soundTimer = 0;
     this.stack = new Array(0x10);
+    this.sp = 0;
     this.paused = 0;
     this.speed = 10;
   }
@@ -30,11 +31,34 @@ export default class Cpu {
 
   cycle = (opcode) => {
     this.carry = 0xf;
+    // get the 12 lowest bits of opcode
     this.nnn = opcode & 0x0fff;
+    //get the 8 lowest bits of opcode
     this.nn = opcode & 0x00ff;
+    // get the lowest nibble of opcode
     this.n = opcode & 0x000f;
     this.vx = (opcode & 0x0f00) >> 8;
     this.vy = (opcode & 0x00f0) >> 4;
+    
+    switch (opcode & 0xf000) {
+      case 0x0000:
+        switch (opcode) {
+          case 0x00e0:
+          // CLS clear the screen;
+            for (let i = 0; i < this.display.length; i++) {
+              this.display[i] = false;
+            }
+            break;
+          case 0x00ee:
+          // RET set pc to address at the top of stack
+          this.sp--;
+          this.pc = this.stack[this.sp];
+          break;
+        }
+        break;
+      case 0x1000:
+        this.pc = this.nnn;
+    }
   }
 
   setArray = (array, val) => {
--- src/test/cpu.test.js
+++ src/test/cpu.test.js
@@ -2,25 +2,52 @@
 import Cpu from "../chip8/cpu";
 let cpu = new Cpu();
 
-cpu.cycle(0xffff);
-console.log(cpu.n)
+describe('opcode', () => {
+  cpu.cycle(0xffff)
+  test("this.nnn should equal the lowest 12 bits of opcode", () => {
+    expect(cpu.nnn).toEqual(0x0fff);
+  });
+  
+  test("this.nn should equal lowest 8 bits of opcode", () => {
+    expect(cpu.nn).toEqual(0x00ff);
+  });
+  
+  test('this.nib should equal lowest 4 bits of opcode', () => {
+    expect(cpu.n).toEqual(0x000f);
+  });
+  
+  test('this.vx should = 15', () => {
+    expect(cpu.vx).toEqual(15);
+  });
+  
+  test('this.vy should = 15', () => {
+    expect(cpu.vy).toEqual(15);
+  });
+})
 
-test("this.nnn should equal the lowest 12 bits of opcode", () => {
-  expect(cpu.nnn).toEqual(0x0fff);
+
+
+test('CLS instruction should clear the screen', () => {
+cpu.cycle(0x00e0);
+  for (let i = 0; i < cpu.display.length; ++i) {
+    expect(cpu.display).toEqual(expect.arrayContaining([false]));
+  };
 });
 
-test("this.nn should equal lowest 8 bits of opcode", () => {
-  expect(cpu.nn).toEqual(0x00ff);
+test('Ret should set pc to address at top of stack', () => {
+  cpu.sp = 2;
+  cpu.stack[1] = 0x999;
+  cpu.cycle(0x00ee);
+  expect(cpu.pc).toEqual(0x999);
+})
+
+test('Ret should decrement the stack pointer', () => {
+  cpu.sp = 2;
+  cpu.cycle(0x00ee);
+  expect(cpu.sp).toEqual(1);
 });
 
-test('this.nib should equal lowest 4 bits of opcode', () => {
-  expect(cpu.n).toEqual(0x000f);
-});
-
-test('this.vx should = 15', () => {
-  expect(cpu.vx).toEqual(15);
-});
-
-test('this.vy should = 15', () => {
-  expect(cpu.vy).toEqual(15);
-});
+test('JP should set pc to designated address', () => {
+  cpu.cycle(0x1333);
+  expect(cpu.pc).toEqual(0x333);
+})
