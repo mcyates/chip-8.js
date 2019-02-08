0501113a28f8ac2bbd2b12de9df14fa2a42b091a
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-02-08 17:20:03 -0500

    implemented more opcodes with tests

--- src/chip8/cpu.js
+++ src/chip8/cpu.js
@@ -1,11 +1,47 @@
 export default class Cpu {
   constructor() {
-    this.memory = new Uint8Array(4096);
+    this.fontSet = new Uint8Array([
+      0xf0, 0x90, 0x90, 0x90, 0xf0, // 0
+      0x20, 0x60, 0x20, 0x20, 0x70, // 1
+      0xf0, 0x10, 0xf0, 0x80, 0xf0, // 2
+      0xf0, 0x10, 0xf0, 0x10, 0xf0, // 3
+      0x90, 0x90, 0xf0, 0x10, 0x10, // 4
+      0xf0, 0x80, 0xf0, 0x10, 0xf0, // 5
+      0xf0, 0x80, 0xf0, 0x90, 0xf0, // 6
+      0xf0, 0x10, 0x20, 0x40, 0x40, // 7
+      0xf0, 0x90, 0xf0, 0x90, 0xf0, // 8
+      0xf0, 0x90, 0xf0, 0x10, 0xf0, // 9
+      0xf0, 0x90, 0xf0, 0x90, 0x90, // A
+      0xe0, 0x90, 0xe0, 0x90, 0xe0, // B
+      0xf0, 0x80, 0x80, 0x80, 0xf0, // C
+      0xe0, 0x90, 0x90, 0x90, 0xe0, // D
+      0xf0, 0x80, 0xf0, 0x80, 0xf0, // E
+      0xf0, 0x80, 0xf0, 0x80, 0x80 // F
+    ]);
+    this.keyMap = {
+      49: 0x1,
+      50: 0x2,
+      51: 0x3,
+      52: 0xc,
+      81: 0x4,
+      87: 0x5,
+      69: 0x6,
+      82: 0x13,
+      65: 0x7,
+      83: 0x8,
+      68: 0x9,
+      70: 0x14,
+      90: 0x10,
+      88: 0x0,
+      67: 0x11,
+      86: 0x15
+    };
+    this.memory = new Uint8Array(0x1000);
     this.pc = 0x200;
     this.iR = 0;
     this.display = this.setArray(new Array(0x800), false);
-    this.input = this.setArray(new Array(16), false)
-    this.vR = new Uint8Array(0x10);
+    this.input = this.setArray(new Array(0x10), false);
+    this.v = new Uint8Array(0x10);
     this.delayTimer = 0;
     this.soundTimer = 0;
     this.stack = new Array(0x10);
@@ -14,58 +50,186 @@ export default class Cpu {
     this.speed = 10;
   }
 
-  loadRom = (rom)  => {
+  loadRom = rom => {
     let length = rom.length;
     for (let i = 0; i < program.length; i++) {
       this.memory[0x200 + i] = rom[i];
     }
-  }
+  };
   run = () => {
     for (let i = 0; i < this.speed; ++i) {
       if (!this.paused) {
         this.opcode = (this.memory[this.pc] << 8) | this.memory[this.pc + 1];
         this.cycle(this.opcode);
+        this.pc += 2;
       }
     }
-  }
+  };
 
-  cycle = (opcode) => {
-    this.carry = 0xf;
+  cycle = opcode => {
     // get the 12 lowest bits of opcode
     this.nnn = opcode & 0x0fff;
     //get the 8 lowest bits of opcode
     this.nn = opcode & 0x00ff;
     // get the lowest nibble of opcode
     this.n = opcode & 0x000f;
-    this.vx = (opcode & 0x0f00) >> 8;
-    this.vy = (opcode & 0x00f0) >> 4;
-    
+    // the lower 4 bits of the high byte of the instruction
+    this.x = (opcode & 0x0f00) >> 8;
+    // upper 4 bits of the low byte of the instruction
+    this.y = (opcode & 0x00f0) >> 4;
+
     switch (opcode & 0xf000) {
       case 0x0000:
         switch (opcode) {
           case 0x00e0:
-          // CLS clear the screen;
+            // CLS clear the screen;
             for (let i = 0; i < this.display.length; i++) {
               this.display[i] = false;
             }
             break;
           case 0x00ee:
-          // RET set pc to address at the top of stack
-          this.sp--;
-          this.pc = this.stack[this.sp];
-          break;
+            // RET set pc to address at the top of stack
+            this.sp--;
+            this.pc = this.stack[this.sp];
+            break;
         }
         break;
       case 0x1000:
+        // JP addr 1nnn sets pc to nnn
         this.pc = this.nnn;
+        break;
+      case 0x2000:
+        // CALL addr 2nnn increment sp then puts current pc on top of stack pc set to nnn
+        this.stack[this.sp] = this.pc;
+        this.sp++;
+        this.pc = this.nnn;
+        break;
+      case 0x3000:
+        // SE Vx, byte 3xkk skip next instruction if V[x] = nn
+        if (this.v[this.x] === this.nn) {
+          this.pc += 2;
+        } 
+        break;
+      case 0x4000:
+        // SNE Vx, byte 4xkk compare v[x] to nn advance pc if not equal
+        if (this.v[this.x] !== this.nn) {
+          this.pc += 2;
+        }
+        break;
+      case 0x5000:
+      // SE V[x], V[y] 5xy0 compare v[x] to v[y] advance pc if equal
+        if (this.v[this.x] === this.v[this.y]) {
+          this.pc += 2;
+        }
+        break;
+      case 0x6000:
+        // LD v[x], byte 6xnn put nn into register v[x]
+        this.v[this.x] = this.nn;
+        break;
+      case 0x7000:
+        // ADD v[x], Byte add nn + v[x] to v[x]
+        this.v[this.x] = this.v[this.x] + this.nn;
+        break;
+      case 0x8000:
+        switch (opcode & 0x000f) {
+          case 0x0000:
+            // LD v[x], v[y] 8xy0 store v[y] in v[x]
+            this.v[this.x] = this.v[this.y]; 
+            break;
+          case 0x0001:
+            // OR v[x], v[y] 8xy1 stores v[x] | v[y] in v[x];
+            this.v[this.x] = (this.v[this.x] | this.v[this.y]);
+            break;
+          case 0x0002:
+            // AND v[x], v[y] 8xy2 stores v[x] & v[y] in v[x];
+            this.v[this.x] = this.v[this.x] & this.v[this.y];
+            break;
+          case 0x0003:
+            // XOR v[x], v[y] 8xy3 stores v[x] ^ v[y] in v[x];
+            this.v[this.x] = this.v[this.x] ^ this.v[this.y];
+            break;
+          case 0x0004:
+            // ADD v[x], v[y] 8xy4 v[x] + v[y] if result > 255 v[f] = 1 else 0
+            // lowest 8-bits are then stored in v[x];
+            let sum = this.v[this.x] + this.v[this.y];
+            if (sum > 0xff) {
+              this.v[0xf] = 1;
+            } else {
+              this.v[0xf] = 0;
+            }
+            this.v[this.x] = sum;
+            break;
+          case 0x0005:
+            // SUB v[x], v[y] 8xy5 v[x] = v[x] - v[y]
+            if (this.v[this.x] > this.v[this.y]) {
+              this.v[0xf] = 1;
+            } else {
+              this.v[0xf] = 0;
+            }
+            this.v[this.x] = this.v[this.x] - this.v[this.y];
+            break;
+          case 0x0006:
+            // SHR v[x], v[y] 8xy6 divide v[x] \ 2
+            // if lowest bit of v[x] === 1 then vf = 1
+            this.v[0xf] = this.v[this.x] & 0x01;
+            this.v[this.x] = this.v[this.x] >> 1;
+            break;
+          case 0x0007:
+            // SUB v[x], v[y] 8xy7 v[x] = v[y] - v[x]
+            if (this.v[this.x] > this.v[this.y]) {
+              this.v[0xf] = 0;
+            } else {
+              this.v[0xf] = 1;
+            }
+            this.v[this.x] = this.v[this.y] - this.v[this.x];
+            break;
+          case 0x000e:
+            //  SHL v[x], v[y] set v[x] = v[x] shl 1
+            this.v[0xf] = this.v[this.x] & 0x80;
+            this.v[this.x] = this.v[this.x] << 1;
+            break;
+        }
+        break;
+    }
+
+  };
+
+  displayReset = () => {
+    for (let i = this.display.length; i >= 0; i--) {
+      this.display[i] = false;
     }
   }
 
+  inputReset = () => {
+    for (let i = 0; i < this.input.length; i++) {
+      this.input[i] = false;
+    }
+  }
+
+  loadFont = () => {
+    for (let i = 0; i < this.fontSet.length; i++) {
+      this.memory[i] = this.fontSet[i];
+    }
+  };
+
+  reset = () => {
+    this.delayTimer = 0;
+    this.displayReset();
+    this.iR = 0;
+    this.memory = new Uint8Array(0x1000);
+    this.pc = 0x200;
+    this.soundTimer = 0;
+    this.sp = 0;
+    this.stack = new Array(0x10);
+    this.vR = new Uint8Array(0x10);
+    this.paused = 0;
+    this.loadFont();
+  };
+
   setArray = (array, val) => {
-    
     for (let i = array.length; i >= 0; --i) {
       array[i] = val;
     }
     return array;
-  }
+  };
 }
--- src/test/cpu.test.js
+++ src/test/cpu.test.js
@@ -2,52 +2,267 @@
 import Cpu from "../chip8/cpu";
 let cpu = new Cpu();
 
-describe('opcode', () => {
-  cpu.cycle(0xffff)
+afterEach(() => {
+  cpu.reset();
+})
+
+describe("opcode", () => {
+  cpu.cycle(0xffff);
   test("this.nnn should equal the lowest 12 bits of opcode", () => {
     expect(cpu.nnn).toEqual(0x0fff);
   });
-  
+
   test("this.nn should equal lowest 8 bits of opcode", () => {
     expect(cpu.nn).toEqual(0x00ff);
   });
-  
-  test('this.nib should equal lowest 4 bits of opcode', () => {
+
+  test("this.nib should equal lowest 4 bits of opcode", () => {
     expect(cpu.n).toEqual(0x000f);
   });
-  
-  test('this.vx should = 15', () => {
-    expect(cpu.vx).toEqual(15);
+
+  test("this.vx should = 15", () => {
+    expect(cpu.x).toEqual(15);
   });
-  
-  test('this.vy should = 15', () => {
-    expect(cpu.vy).toEqual(15);
+
+  test("this.vy should = 15", () => {
+    expect(cpu.y).toEqual(15);
   });
-})
-
-
-
-test('CLS instruction should clear the screen', () => {
-cpu.cycle(0x00e0);
-  for (let i = 0; i < cpu.display.length; ++i) {
-    expect(cpu.display).toEqual(expect.arrayContaining([false]));
-  };
 });
 
-test('Ret should set pc to address at top of stack', () => {
-  cpu.sp = 2;
-  cpu.stack[1] = 0x999;
+test("CLS instruction should clear the screen", () => {
+  cpu.cycle(0x00e0);
+  for (let i = 0; i < cpu.display.length; ++i) {
+    expect(cpu.display).toEqual(expect.arrayContaining([false]));
+  }
+});
+
+test("Ret should set pc to address at top of stack", () => {
+  cpu.sp = 1;
+  cpu.stack[0] = 0x999;
   cpu.cycle(0x00ee);
   expect(cpu.pc).toEqual(0x999);
-})
+});
 
-test('Ret should decrement the stack pointer', () => {
-  cpu.sp = 2;
+test("Ret should decrement the stack pointer", () => {
+  cpu.sp = 1;
   cpu.cycle(0x00ee);
+  expect(cpu.sp).toEqual(0);
+});
+
+test("JP should set pc to designated address", () => {
+  cpu.cycle(0x1333);
+  expect(cpu.pc).toEqual(0x333);
+});
+
+test('Call should put pc on top of the stack', () => {
+  cpu.cycle(0x2200);
+  expect(cpu.stack[cpu.sp - 1]).toEqual(0x200);
+});
+
+test('Call should increment sp', () => {
+  cpu.cycle(0x2200);
   expect(cpu.sp).toEqual(1);
 });
 
-test('JP should set pc to designated address', () => {
-  cpu.cycle(0x1333);
-  expect(cpu.pc).toEqual(0x333);
-})
+test('CAll should set pc to = nnn', () => {
+  cpu.cycle(0x2400);
+  expect(cpu.pc).toEqual(0x400)
+});
+
+test('SE Vx, byte should increment pc if Vx === nn', () => {
+  cpu.v[5] = 0x0003;
+  cpu.cycle(0x3503)
+  expect(cpu.pc).toEqual(0x202);
+});
+
+test('SNE Vx, byte should increment pc if Vx !== nn', () => {
+  cpu.v[3] = 0x0004;
+  cpu.cycle(0x4333);
+  expect(cpu.pc).toEqual(0x202);
+});
+
+test('SE Vx, Vy should increment pc if Vx === Vy', () => {
+  cpu.v[4] = 3;
+  cpu.v[5] = 3;
+  cpu.cycle(0x5450);
+  expect(cpu.pc).toEqual(0x202);
+});
+
+test('LD vX should put nn into vX', () => {
+  cpu.cycle(0x6433);
+  expect(cpu.v[4]).toEqual(0x0033);
+});
+
+test('v[x] should be the sum of v[x] + nn', () => {
+  cpu.v[5] = 0x0004;
+  cpu.cycle(0x7505);
+  expect(cpu.v[cpu.x]).toEqual(0x0009);
+});
+
+test('8xy0 v[y] should be stored in v[x]', () => {
+  cpu.v[1] = 3;
+  cpu.cycle(0x8210);
+  expect(cpu.v[cpu.x]).toEqual(3);
+});
+describe('8xy1', () => {
+  test('8xy1 0x0040 | 0x0004 should = 0x0044', () => {
+    cpu.v[1] = 0x0040;
+    cpu.v[2] = 0x0004;
+    cpu.cycle(0x8121);
+    expect(cpu.v[cpu.x]).toEqual(0x0044);
+  });
+  test('8xy1 0x0000 | 0x0000 should = 0;]', () => {
+    cpu.v[1] = 0x0000;
+    cpu.v[2] = 0x0000;
+    cpu.cycle(0x8121);
+    expect(cpu.v[cpu.x]).toEqual(0x0000);
+  });
+});
+
+describe('8xy2', () => {
+  test('0x0444 & 0x0044 should = 0x0044', () => {
+    cpu.v[1] = 0x0444;
+    cpu.v[2] = 0x0044;
+    cpu.cycle(0x8122);
+    expect(cpu.v[cpu.x]).toEqual(0x0044);
+  });
+  test('8xy2 0x4400 & 0x0044 should = 0', () => {
+    cpu.v[1] = 0x4400;
+    cpu.v[2] = 0x0044;
+    cpu.cycle(0x8122);
+    expect(cpu.v[cpu.x]).toEqual(0x0000);
+  });
+});
+
+describe('8xy3', () => {
+  test('8xy3 1 ^ 1 should = 0', () => {
+    cpu.v[1] = 1;
+    cpu.v[2] = 1;
+    cpu.cycle(0x8123);
+    expect(cpu.v[cpu.x]).toEqual(0);
+  });
+  test('8xy3 1 ^ 0 should = 1', () => {
+    cpu.v[1] = 0;
+    cpu.v[2] = 1;
+    cpu.cycle(0x8123);
+    expect(cpu.v[cpu.x]).toEqual(1);
+  });
+  test('8xy3 0 ^ 0 should = 0', () => {
+    cpu.v[1] = 0;
+    cpu.v[2] = 0;
+    cpu.cycle(0x8123);
+    expect(cpu.v[cpu.x]).toEqual(0);
+  });
+});
+
+describe('8xy4', () => {
+  test('3 + 4 = 7', () => {
+    cpu.v[1] = 3;
+    cpu.v[2] = 4;
+    cpu.cycle(0x8124);
+    expect(cpu.v[cpu.x]).toEqual(0x0007);
+  });
+  test('sum > 255 v[0xf] = 1', () => {
+    cpu.v[1] = 0xff;
+    cpu.v[2] = 0x01;
+    cpu.cycle(0x8124);
+    expect(cpu.v[0xf]).toEqual(1);
+    expect(cpu.v[cpu.x]).toEqual(0);
+  });
+  test('sum < 255 v[0xf] = 0', () => {
+    cpu.v[1] = 0xfe;
+    cpu.v[2] = 0x01;
+    cpu.cycle(0x8124);
+    expect(cpu.v[1]).toEqual(255);
+    expect(cpu.v[0xf]).toEqual(0);
+  });
+});
+
+describe('8xy5', () => {
+  test('5 - 4 should = 1', () => {
+    cpu.v[1] = 5;
+    cpu.v[2] = 4;
+    cpu.cycle(0x8125);
+    expect(cpu.v[cpu.x]).toEqual(1);
+  });
+  test('5 - 6 should = 255', () => {
+    cpu.v[1] = 5;
+    cpu.v[2] = 6;
+    cpu.v[0xF] = 1;
+    cpu.cycle(0x8125);
+    expect(cpu.v[cpu.x]).toEqual(0xff);
+    expect(cpu.v[0xf]).toEqual(0);
+  });
+  test("v[0xf] should = 1 if there isn't a borrow", () => {
+    cpu.v[1] = 5;
+    cpu.v[2] = 3;
+    cpu.v[0xF] = 0;
+    cpu.cycle(0x8125);
+    expect(cpu.v[cpu.x]).toEqual(2);
+    expect(cpu.v[0xf]).toEqual(1);
+  });
+});
+
+describe('8xy6', () => {
+  test('sets vf to least significant bit', () => {
+    cpu.v[1] = 8;
+    cpu.v[0xF] = 1;
+    cpu.cycle(0x8116)
+    expect(cpu.v[0xf]).toEqual(0);
+
+    cpu.v[1] = 9;
+    cpu.v[0xf] = 0;
+    cpu.cycle(0x8116);
+    expect(cpu.v[0xf]).toEqual(1);
+  });
+  test('shift v[x] right by one', () => {
+    cpu.v[1] = 8;
+    cpu.cycle(0x8116);
+    expect(cpu.v[cpu.x]).toEqual(4);
+  });
+});
+
+describe('8xy7', () => {
+  test('vx = vy - vx', () => {
+    cpu.v[1] = 8
+    cpu.v[2] = 10
+    cpu.cycle(0x8127);
+    expect(cpu.v[cpu.x]).toEqual(2);
+  });
+  test('set v[0xf] to zero when there is a borrow', () => {
+    cpu.v[1] = 11;
+    cpu.v[2] = 10;
+    cpu.v[0xf] = 1;
+    cpu.cycle(0x8127);
+    expect(cpu.v[0xf]).toEqual(0);
+    expect(cpu.v[cpu.x]).toEqual(0xff);
+  });
+  test('set v[0xf] to 0 when there isn\'t a borrow', () => {
+    cpu.v[1] = 10;
+    cpu.v[2] = 10;
+    cpu.v[0xf] = 0;
+    cpu.cycle(0x8127);
+    expect(cpu.v[0xf]).toEqual(1)
+    expect(cpu.v[cpu.x]).toEqual(0)
+  });
+});
+
+describe('8xye', () => {
+  test('shift v[x] left by one', () => {
+    cpu.v[1] = 8;
+    cpu.cycle(0x811e);
+    expect(cpu.v[cpu.x]).toEqual(0x10);
+  });
+  test('set v[0xf] to val of largest bit of v[x] before shift', () => {
+    cpu.v[1] = 0x08;
+    cpu.v[0xf] = 1;
+    cpu.cycle(0x811e);
+    expect(cpu.v[0xf]).toEqual(0);
+
+    cpu.v[1] = 0x88;
+    cpu.v[0xf] = 0;
+    cpu.cycle(0x811e);
+    console.log(cpu.v[0xf])
+    expect(cpu.v[0xf]).toEqual(0x80);
+  });
+});
